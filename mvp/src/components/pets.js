10fe80c42d1e0d3029130f75e337ea8e8ded1fc7
swizzlejuice <bumblefizzzle@gmail.com>
2024-04-09 15:18:14 -0700

    added cat images, etc

--- mvp/src/components/pets.js
+++ mvp/src/components/pets.js
@@ -10,15 +10,27 @@ function Pets() {
         { imgSrc: "toto.png", price: 200 },
         { imgSrc: "ayumi.png", price: 200 },
         { imgSrc: "echo.png", price: 200 },
-        { imgSrc: "simba.png", price: 200 },
+        { imgSrc: "cat1.png", price: 200 },
+        { imgSrc: "cat2.png", price: 200 },
+        { imgSrc: "cat3.png", price: 200 },
+        { imgSrc: "cat4.png", price: 200 },
+        { imgSrc: "cat5.png", price: 200 },
     ];
+
+    const firstHalf = items.slice(0, Math.ceil(items.length / 2));
+    const secondHalf = items.slice(Math.ceil(items.length / 2));
 
     return (
         <div className="store-page">
             <div className="container">
                 <StoreNavigator />
                 <div className="row mt-5 justify-content-center">
-                    {items.map(item => (
+                    {firstHalf.map(item => (
+                        <PetBox key={item.itemName} imgSrc={item.imgSrc} itemName={item.itemName} price={item.price} />
+                    ))}
+                </div>
+                <div className="row mt-5 justify-content-center">
+                    {secondHalf.map(item => (
                         <PetBox key={item.itemName} imgSrc={item.imgSrc} itemName={item.itemName} price={item.price} />
                     ))}
                 </div>
