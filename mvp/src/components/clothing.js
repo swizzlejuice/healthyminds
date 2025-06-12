9b41da053fc239df2f44a9442aa3253e61ed8e8b
swizzlejuice <bumblefizzzle@gmail.com>
2025-06-12 15:03:58 -0700

    reordering

--- mvp/src/components/clothing.js
+++ mvp/src/components/clothing.js
@@ -5,13 +5,13 @@ import ItemBox from './itemBox';
 function StorePage() {
     const items = [
         { imgSrc: "basictee.png", itemimage: "basictee", itemName: "Basic Tee", price: 35, isNecessity: false },
+        { imgSrc: "loungetee.png", itemName: "Lounge Tee", price: 40, isNecessity: false },
+        { imgSrc: "baggysweater.png", itemName: "Baggy Sweater", price: 45, isNecessity: false },
+        { imgSrc: "softtee.png", itemName: "Soft Tee", price: 50, isNecessity: false },
         { imgSrc: "orangepolo.png", itemName: "Orange Polo", price: 55, isNecessity: false },
         { imgSrc: "onsie.png", itemName: "The Onesie", price: 65, isNecessity: false },
         { imgSrc: "lavenderwrap.png", itemName: "Lavender Wrap", price: 75, isNecessity: false },
-        { imgSrc: "baggysweater.png", itemName: "Baggy Sweater", price: 45, isNecessity: false },
         { imgSrc: "redpuffer.png", itemName: "Red Puffer", price: 85, isNecessity: false},
-        { imgSrc: "softtee.png", itemName: "Soft Tee", price: 50, isNecessity: false },
-        { imgSrc: "loungetee.png", itemName: "Lounge Tee", price: 40, isNecessity: false },
         // all vectors sourced from Vecteezy with a premium license
     ];
 
